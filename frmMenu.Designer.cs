3b66e5e0807ee109b15b76d2058aa9066b01d5ae
tiamatinc <smadisonwaits@gmail.com>
2017-04-27 19:46:18 -0700

    blammo

---
+++ frmMenu.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RecipeMadness1
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnAddRecipe = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(127, 126);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(88, 66);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find Recipe";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAddRecipe
+            // 
+            this.btnAddRecipe.Location = new System.Drawing.Point(127, 198);
+            this.btnAddRecipe.Name = "btnAddRecipe";
+            this.btnAddRecipe.Size = new System.Drawing.Size(88, 66);
+            this.btnAddRecipe.TabIndex = 1;
+            this.btnAddRecipe.Text = "Add Recipe";
+            this.btnAddRecipe.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(127, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 66);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Favorites";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Poor Richard", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(28, 37);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(294, 31);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Find and Store Your Recipes";
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 404);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnAddRecipe);
+            this.Controls.Add(this.btnFind);
+            this.Name = "frmMenu";
+            this.Text = "Recipes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnAddRecipe;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
