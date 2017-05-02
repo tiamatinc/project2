ba99f542b6f537aaf47791a2eccb80ec30694ba4
Serenity Waits <smwaits@cpp.edu>
2017-05-01 21:41:21 -0700

    ..yes?

---
+++ frmShowResults.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace RecipeMadness1
+{
+    partial class frmShowResults
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            this.recipeDBDataSet = new RecipeMadness1.RecipeDBDataSet();
+            this.recipeDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recipeDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recipeDBDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.AllowUserToAddRows = false;
+            this.dgvResults.AllowUserToDeleteRows = false;
+            this.dgvResults.AllowUserToOrderColumns = true;
+            this.dgvResults.AutoGenerateColumns = false;
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.DataSource = this.recipeDBDataSetBindingSource;
+            this.dgvResults.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvResults.Location = new System.Drawing.Point(28, 35);
+            this.dgvResults.MultiSelect = false;
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.ReadOnly = true;
+            this.dgvResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvResults.Size = new System.Drawing.Size(691, 306);
+            this.dgvResults.TabIndex = 0;
+            // 
+            // recipeDBDataSet
+            // 
+            this.recipeDBDataSet.DataSetName = "RecipeDBDataSet";
+            this.recipeDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // recipeDBDataSetBindingSource
+            // 
+            this.recipeDBDataSetBindingSource.DataSource = this.recipeDBDataSet;
+            this.recipeDBDataSetBindingSource.Position = 0;
+            // 
+            // frmShowResults
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 439);
+            this.Controls.Add(this.dgvResults);
+            this.Name = "frmShowResults";
+            this.Text = "frmShowResults";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recipeDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recipeDBDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvResults;
+        private System.Windows.Forms.BindingSource recipeDBDataSetBindingSource;
+        private RecipeDBDataSet recipeDBDataSet;
+    }
+}
